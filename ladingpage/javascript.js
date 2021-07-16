237c9d7e9644cfa119e49a9842beeb906944f88b
Goto <mateusgoto01@gmail.com>
2021-07-16 12:14:15 -0300

    Add Validations

--- ladingpage/javascript.js
+++ ladingpage/javascript.js
@@ -1,3 +1,22 @@
+class validator{
+
+    constructor(){
+        this.validations = [
+
+        ]
+    }
+    validate(form){
+        let inputs = form.getElementByTagName('input');
+
+        let inputs_array = [...inputs];
+
+        inputs_array.forEach(function(input){
+            
+        }, this);
+    }
+}
+
+
 function storageData(){
     var name = document.getElementById('dataName').value;
     var email = document.getElementById('dataEmail').value;
@@ -6,3 +25,13 @@ function storageData(){
     localStorage.setItem('Email', email);
     localStorage.setItem('Game', game);
 }
+
+
+let submit = document.getElementById('button');
+let form = document.getElementById('box');
+let validator = new validator;
+submit.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+});
